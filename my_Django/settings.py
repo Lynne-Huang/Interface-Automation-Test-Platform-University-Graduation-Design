5c3201a9ee6994495f20b200ff4a6130f994b68a
Lynne-Huang <303590919@qq.com>
2019-03-17 22:03:38 +0800

    加入bootstrap 4

--- my_Django/settings.py
+++ my_Django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "bootstrap"),
+)
+
+
